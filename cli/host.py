97e5596188581745575369766c5b9f67605f2324
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-05-31 16:17:03 +0300

    SKALE-1107 fix docker installation script

--- cli/host.py
+++ cli/host.py
@@ -1,6 +1,11 @@
+import os
 import subprocess
 from cli.config import DEPENDENCIES_SCRIPT
 
 def install_host_dependencies():
-    res = subprocess.run(["bash", DEPENDENCIES_SCRIPT])
+    env = {
+        **os.environ,
+        'SKALE_CMD': 'host_deps'
+    }
+    res = subprocess.run(["sudo", "bash", DEPENDENCIES_SCRIPT], env=env)
     # todo: check execution status
