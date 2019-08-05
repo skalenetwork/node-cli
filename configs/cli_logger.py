2894ee88445b1b8b404ee2a0f8d2a2e9adec661d
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-08-05 16:05:58 +0300

    SKALE-1412 add cli logger module

---
+++ configs/cli_logger.py
@@ -0,0 +1,16 @@
+import os
+from configs.node import NODE_DATA_PATH
+
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+LOG_FILE_SIZE_MB = 300
+LOG_FILE_SIZE_BYTES = LOG_FILE_SIZE_MB * 1000000
+
+LOG_BACKUP_COUNT = 1
+
+LOG_DATA_PATH = os.path.join(NODE_DATA_PATH, 'log')
+
+LOG_FILENAME = 'node-cli.log'
+DEBUG_LOG_FILENAME = 'debug-node-cli.log'
+LOG_FILEPATH = os.path.join(LOG_DATA_PATH, LOG_FILENAME)
+DEBUG_LOG_FILEPATH = os.path.join(LOG_DATA_PATH, DEBUG_LOG_FILENAME)
