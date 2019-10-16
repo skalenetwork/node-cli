6e8914c43c24f9a88b85bb34a5555000d4fe5887
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-10-16 16:12:45 +0300

    SKALE-1653 fix update script

--- datafiles/update_node_project.sh
+++ datafiles/update_node_project.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-REPO_NAME=skale-node
+SKALE_DIR=~/.skale
+SKALE_NODE_DIR_NAME=.skale-node
+SKALE_NODE_DIR="$SKALE_DIR"/"$SKALE_NODE_DIR_NAME"
 
-cd /skale/$REPO_NAME
+cd $SKALE_NODE_DIR
 sudo git pull
