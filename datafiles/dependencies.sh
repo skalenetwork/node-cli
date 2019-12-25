450fa3a6f5884c86a57d23fc6147f0dec1ac1441
badrogger <ivan@skalelabs.com>
2019-12-25 12:56:21 +0200

    Uncomment docker installation

--- datafiles/dependencies.sh
+++ datafiles/dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sh get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
