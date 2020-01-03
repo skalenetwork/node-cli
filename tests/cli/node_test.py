0e89aa106b41fd15535984bed9ef6c9aa22b08c4
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-01-03 16:05:13 +0200

    hotfix: fix post request in node_register

--- tests/cli/node_test.py
+++ tests/cli/node_test.py
@@ -28,7 +28,7 @@ from cli.node import (register_node, init_node, purge_node, update_node,
 def test_register_node(skip_auth, config):
     resp_mock = response_mock(requests.codes.created)
     result = run_command_mock(
-        'core.node.post_request',
+        'core.node.post',
         resp_mock,
         register_node,
         ['--name', 'test-node', '--ip', '0.0.0.0', '--port', '8080'])
@@ -36,7 +36,7 @@ def test_register_node(skip_auth, config):
     assert result.output == 'Node registered in SKALE manager. For more info run: skale node info\n'
 
     result = run_command_mock(
-        'core.node.post_request',
+        'core.node.post',
         None,
         register_node,
         ['--name', 'test-node2', '--ip', '0.0.0.0', '--port', '80'])
@@ -46,7 +46,7 @@ def test_register_node(skip_auth, config):
     resp_mock = response_mock(requests.codes.ok,
                               json_data={'errors': ['Strange error']})
     result = run_command_mock(
-        'core.node.post_request',
+        'core.node.post',
         resp_mock,
         register_node,
         ['--name', 'test-node2', '--ip', '0.0.0.0', '--port', '80'])
@@ -57,7 +57,7 @@ def test_register_node(skip_auth, config):
 def test_register_node_with_prompted_ip(config, skip_auth):
     resp_mock = response_mock(requests.codes.created)
     result = run_command_mock(
-        'core.node.post_request',
+        'core.node.post',
         resp_mock,
         register_node,
         ['--name', 'test-node', '--port', '8080'], input='0.0.0.0\n')
@@ -68,7 +68,7 @@ def test_register_node_with_prompted_ip(config, skip_auth):
 def test_register_node_with_default_port_and_name(config, skip_auth):
     resp_mock = response_mock(requests.codes.created)
     result = run_command_mock(
-        'core.node.post_request',
+        'core.node.post',
         resp_mock,
         register_node,
         input='0.0.0.0\n')
@@ -83,7 +83,7 @@ def test_init_node(skip_local_only, config):
             mock.patch('core.node.prepare_host'), \
             mock.patch('core.node.init_data_dir'):
         result = run_command_mock(
-            'core.node.post_request',
+            'core.node.post',
             resp_mock,
             init_node,
             ['--env-file', './tests/test-env'],
@@ -97,7 +97,7 @@ def test_purge(skip_local_only, config):
     resp_mock = response_mock(requests.codes.created)
     with mock.patch('core.node.subprocess.run'):
         result = run_command_mock(
-            'core.node.post_request',
+            'core.node.post',
             resp_mock,
             purge_node,
             params
@@ -114,7 +114,7 @@ def test_update_node(skip_local_only, config):
             mock.patch('core.node.prepare_host'), \
             mock.patch('core.node.init_data_dir'):
         result = run_command_mock(
-            'core.node.post_request',
+            'core.node.post',
             resp_mock,
             update_node,
             params,
