80f4596d06f23ee9f9c120952d7c3a37aecf0701
ivd <ivan@skalelabs.com>
2020-02-07 16:06:30 +0200

    Comment installing iptables-persistent

--- datafiles/dependencies.sh
+++ datafiles/dependencies.sh
@@ -5,7 +5,7 @@ sh get-docker.sh
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo apt install iptables-persistent
+# sudo apt install iptables-persistent -y
 
 
 while ! (ps -ef | grep "[d]ocker" | awk {'print $2'});
