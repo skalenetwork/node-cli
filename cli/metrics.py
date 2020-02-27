22374db8662dfc080c20109a3fa9491c87ee2511
Alex Sheverdin <alex@skalelabs.com>
2020-02-27 21:15:16 +0200

    SKALE-2177 Update licence copyright date

--- cli/metrics.py
+++ cli/metrics.py
@@ -2,7 +2,7 @@
 #
 #   This file is part of skale-node-cli
 #
-#   Copyright (C) 2019 SKALE Labs
+#   Copyright (C) 2019-2020 SKALE Labs
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU Affero General Public License as published by
