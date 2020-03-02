ecd71b63bcaf16a6f59b9d8e94fb9194a7ef0431
Alex Sheverdin <alex@skalelabs.com>
2020-03-02 19:15:41 +0200

    SKALE-2265 Add more metrics tests

--- tests/cli/metrics_test.py
+++ tests/cli/metrics_test.py
@@ -2,7 +2,7 @@
 #
 #   This file is part of skale-node-cli
 #
-#   Copyright (C) 2019 SKALE Labs
+#   Copyright (C) 2019-2020 SKALE Labs
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU Affero General Public License as published by
@@ -53,12 +53,13 @@ def test_metrics(config):
                       f'+---------------------------------------------------------------------+\n'\
                       f'Total bounty per the given period: 4018.776 SKL\n'
     assert result.output == expected_output
+
+    # test empty results
     resp_mock = response_mock(
         requests.codes.ok,
         json_data={'data': {'metrics': [], 'total': 0}, 'res': 1}
     )
     result = run_command_mock('core.helper.get_request', resp_mock, metrics)
-    print(result.output)
     assert result.exit_code == 0
     expected_output = f'Please wait - collecting metrics data from blockchain...\n' \
                       f'+------------------------------------+\n'\
@@ -66,4 +67,76 @@ def test_metrics(config):
                       f'+------------------------------------+\n' \
                       f'+------------------------------------+\n' \
                       f'Total bounty per the given period: 0.000 SKL\n'
+    assert result.output == expected_output
+
+    # test --wei
+    resp_mock = response_mock(
+        requests.codes.ok,
+        json_data={'data': response_data, 'res': 1}
+    )
+    result = run_command_mock('core.helper.get_request', resp_mock, metrics, ['--wei'])
+    assert result.exit_code == 0
+
+    expected_output = f'Please wait - collecting metrics data from blockchain...\n' \
+                      f'+-------------------------------------------------------------------+\n' \
+                      f'|        Date                   Bounty           Downtime   Latency |\n' \
+                      f'+-------------------------------------------------------------------+\n' \
+                      f'| 2019-10-09 02:46:50   4018775720164608966656          0       1.0 |\n' \
+                      f'| 2019-10-09 03:47:00   4018775720164608966656          0       1.0 |\n' \
+                      f'| 2019-10-09 04:47:11   4018775720164608966656          0       1.0 |\n' \
+                      f'| 2019-10-09 05:47:21   4018775720164608966656          0       1.0 |\n' \
+                      f'| 2019-10-09 06:47:32   4018775720164608966656          0       1.0 |\n' \
+                      f'+-------------------------------------------------------------------+\n' \
+                      f'Total bounty per the given period: 4018.7759 wei\n'
+    assert result.output == expected_output
+
+    # test --fast
+    resp_mock = response_mock(
+        requests.codes.ok,
+        json_data={'data': response_data, 'res': 1}
+    )
+    result = run_command_mock('core.helper.get_request', resp_mock, metrics, ['--fast'])
+    assert result.exit_code == 0
+
+    expected_output = f'+---------------------------------------------------------------------+\n'\
+                      f'|        Date                    Bounty            Downtime   Latency |\n'\
+                      f'+---------------------------------------------------------------------+\n'\
+                      f'| 2019-10-09 02:46:50   4018775720164608966656.0          0       1.0 |\n'\
+                      f'| 2019-10-09 03:47:00   4018775720164608966656.0          0       1.0 |\n'\
+                      f'| 2019-10-09 04:47:11   4018775720164608966656.0          0       1.0 |\n'\
+                      f'| 2019-10-09 05:47:21   4018775720164608966656.0          0       1.0 |\n'\
+                      f'| 2019-10-09 06:47:32   4018775720164608966656.0          0       1.0 |\n'\
+                      f'+---------------------------------------------------------------------+\n'\
+                      f'Total bounty per the given period: 4018.776 SKL\n'
+    assert result.output == expected_output
+
+    # test --fast and --wei
+    resp_mock = response_mock(
+        requests.codes.ok,
+        json_data={'data': response_data, 'res': 1}
+    )
+    result = run_command_mock('core.helper.get_request', resp_mock, metrics, ['--wei', '--fast'])
+    assert result.exit_code == 0
+
+    expected_output = f'+-------------------------------------------------------------------+\n' \
+                      f'|        Date                   Bounty           Downtime   Latency |\n' \
+                      f'+-------------------------------------------------------------------+\n' \
+                      f'| 2019-10-09 02:46:50   4018775720164608966656          0       1.0 |\n' \
+                      f'| 2019-10-09 03:47:00   4018775720164608966656          0       1.0 |\n' \
+                      f'| 2019-10-09 04:47:11   4018775720164608966656          0       1.0 |\n' \
+                      f'| 2019-10-09 05:47:21   4018775720164608966656          0       1.0 |\n' \
+                      f'| 2019-10-09 06:47:32   4018775720164608966656          0       1.0 |\n' \
+                      f'+-------------------------------------------------------------------+\n' \
+                      f'Total bounty per the given period: 4018.7759 wei\n'
+    assert result.output == expected_output
+
+    # test all parameters
+    resp_mock = response_mock(
+        requests.codes.ok,
+        json_data={'data': response_data, 'res': 1}
+    )
+    result = run_command_mock('core.helper.get_request', resp_mock, metrics,
+                              ['--since', '2019-10-09', '--till', '2019-10-10', '--limit', 5,
+                               '--wei', '--fast'])
+    assert result.exit_code == 0
     assert result.output == expected_output
