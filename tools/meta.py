a5264765f4dd9cd2be7a4e7e3bbfd1a38653353c
badrogger <ivan@skalelabs.com>
2020-11-05 14:50:54 +0200

    SKALE-3445 Add tools.meta module

---
+++ tools/meta.py
@@ -0,0 +1,34 @@
+import json
+import os
+from configs import META_FILEPATH
+
+DEFAULT_VERSION = '1.0.0'
+
+
+def get_meta_info() -> dict:
+    if not os.path.isfile(META_FILEPATH):
+        return {}
+    with open(META_FILEPATH) as meta_file:
+        meta = json.load(meta_file)
+    return meta
+
+
+def save_meta(meta: dict) -> None:
+    with open(META_FILEPATH) as meta_file:
+        json.dump(meta, meta_file)
+
+
+def get_default_meta() -> dict:
+    return compose_meta(DEFAULT_VERSION)
+
+
+def enure_meta(meta: dict = None) -> None:
+    if not get_meta_info():
+        meta = meta or get_default_meta()
+        save_meta(meta)
+
+
+def compose_meta(version: str) -> dict:
+    return {
+        'version': version
+    }
