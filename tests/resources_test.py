4cbd12346dbc0ae176a19c3141607d80f103e033
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-11-18 14:27:09 +0200

    SKALE-3568 Update resources tests

--- tests/resources_test.py
+++ tests/resources_test.py
@@ -5,7 +5,7 @@ from core.resources import (generate_resource_allocation_config, get_schain_volu
 from core.helper import safe_load_yml
 from configs import ALLOCATION_FILEPATH
 
-SCHAIN_VOLUME_PARTS = {'test4': {'max_consensus_storage_bytes': 4, 'max_skaled_leveldb_storage_bytes': 4, 'max_file_storage_bytes': 4, 'max_reserved_storage_bytes': 1}, 'test': {'max_consensus_storage_bytes': 4, 'max_skaled_leveldb_storage_bytes': 4, 'max_file_storage_bytes': 4, 'max_reserved_storage_bytes': 1}, 'small': {'max_consensus_storage_bytes': 0, 'max_skaled_leveldb_storage_bytes': 0, 'max_file_storage_bytes': 0, 'max_reserved_storage_bytes': 0}, 'medium': {'max_consensus_storage_bytes': 4, 'max_skaled_leveldb_storage_bytes': 4, 'max_file_storage_bytes': 4, 'max_reserved_storage_bytes': 1}, 'large': {'max_consensus_storage_bytes': 38, 'max_skaled_leveldb_storage_bytes': 38, 'max_file_storage_bytes': 38, 'max_reserved_storage_bytes': 12}}  # noqa
+SCHAIN_VOLUME_PARTS = {'test4': {'max_consensus_storage_bytes': 1, 'max_file_storage_bytes': 1, 'max_reserved_storage_bytes': 0, 'max_skaled_leveldb_storage_bytes': 1}, 'test': {'max_consensus_storage_bytes': 1, 'max_file_storage_bytes': 1, 'max_reserved_storage_bytes': 0, 'max_skaled_leveldb_storage_bytes': 1}, 'small': {'max_consensus_storage_bytes': 0, 'max_skaled_leveldb_storage_bytes': 0, 'max_file_storage_bytes': 0, 'max_reserved_storage_bytes': 0}, 'medium': {'max_consensus_storage_bytes': 1, 'max_file_storage_bytes': 1, 'max_reserved_storage_bytes': 0, 'max_skaled_leveldb_storage_bytes': 1}, 'large': {'max_consensus_storage_bytes': 38, 'max_skaled_leveldb_storage_bytes': 38, 'max_file_storage_bytes': 38, 'max_reserved_storage_bytes': 12}}  # noqa
 
 
 def disk_alloc_mock():
@@ -25,10 +25,10 @@ def test_generate_resource_allocation_config():
                     new=disk_alloc_mock):
         resource_allocation_config = generate_resource_allocation_config()
 
-        assert resource_allocation_config['schain']['cpu_shares']['test4'] == 89
-        assert resource_allocation_config['schain']['cpu_shares']['test'] == 89
+        assert resource_allocation_config['schain']['cpu_shares']['test4'] == 22
+        assert resource_allocation_config['schain']['cpu_shares']['test'] == 22
         assert resource_allocation_config['schain']['cpu_shares']['small'] == 5
-        assert resource_allocation_config['schain']['cpu_shares']['medium'] == 89
+        assert resource_allocation_config['schain']['cpu_shares']['medium'] == 22
         assert resource_allocation_config['schain']['cpu_shares']['large'] == 716
 
         assert isinstance(resource_allocation_config['schain']['mem']['test4'], int)
@@ -37,13 +37,13 @@ def test_generate_resource_allocation_config():
         assert isinstance(resource_allocation_config['schain']['mem']['medium'], int)
         assert isinstance(resource_allocation_config['schain']['mem']['large'], int)
 
-        assert resource_allocation_config['schain']['disk']['test4'] == 16
-        assert resource_allocation_config['schain']['disk']['test'] == 16
+        assert resource_allocation_config['schain']['disk']['test4'] == 4
+        assert resource_allocation_config['schain']['disk']['test'] == 4
         assert resource_allocation_config['schain']['disk']['small'] == 1
-        assert resource_allocation_config['schain']['disk']['medium'] == 16
+        assert resource_allocation_config['schain']['disk']['medium'] == 4
         assert resource_allocation_config['schain']['disk']['large'] == 128
 
-        assert resource_allocation_config['ima']['cpu_shares'] == {'test4': 38, 'test': 38, 'small': 2, 'medium': 38, 'large': 307} # noqa
+        assert resource_allocation_config['ima']['cpu_shares'] == {'test4': 9, 'test': 9, 'small': 2, 'medium': 9, 'large': 307} # noqa
         assert isinstance(resource_allocation_config['ima']['mem'], dict)
 
         assert resource_allocation_config['schain']['volume_limits'] == SCHAIN_VOLUME_PARTS
@@ -51,6 +51,6 @@ def test_generate_resource_allocation_config():
             'test4': 4294967296,
             'test': 4294967296,
             'small': 4294967296,
-            'medium': 68719476736,
+            'medium': 17179869184,
             'large': 549755813888
         }
