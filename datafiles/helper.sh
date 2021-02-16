85d967102cd20d4b38f38df60639999d831b6216
badrogger <ivan@skalelabs.com>
2021-02-16 12:09:53 +0200

    SKALE-3887 Turn off sla

--- datafiles/helper.sh
+++ datafiles/helper.sh
@@ -10,7 +10,7 @@ export FILESTORAGE_ARTIFACTS_FILE=$NODE_DATA_DIR/filestorage_artifacts.json
 export CONTRACTS_DIR="$SKALE_DIR"/contracts_info
 export BACKUP_CONTRACTS_DIR="$SKALE_DIR"/.old_contracts_info
 
-export BASE_SERVICES="transaction-manager skale-admin skale-api mysql sla bounty nginx watchdog filebeat"
+export BASE_SERVICES="transaction-manager skale-admin skale-api mysql bounty nginx watchdog filebeat"
 export NOTIFICATION_SERVICES="celery redis"
 
 remove_dynamic_containers () {
@@ -36,9 +36,9 @@ remove_dynamic_containers () {
 remove_compose_containers () {
     echo 'Removing node containers ...'
     COMPOSE_PATH=$SKALE_DIR/config/docker-compose.yml
-    echo 'Removing api, sla, bounty, admin containers ...'
-    DB_PORT=0 docker-compose -f  $COMPOSE_PATH rm  -s -f skale-api sla bounty skale-admin
-    echo 'Api, sla, bounty, admin containers were removed. Sleeping ...'
+    echo 'Removing api, bounty, admin containers ...'
+    DB_PORT=0 docker-compose -f  $COMPOSE_PATH rm  -s -f skale-api bounty skale-admin
+    echo 'Api, bounty, admin containers were removed. Sleeping ...'
     sleep 7
     echo 'Removing transaction-manager, mysql and rest ...'
     DB_PORT=0 docker-compose -f $COMPOSE_PATH  rm  -s -f
