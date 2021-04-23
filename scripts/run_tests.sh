47450ea767f4028a13dff36d5316d85ac587ccc4
Dmytro <dmitry.tkk@gmail.com>
2021-04-23 17:28:12 +0300

    SKALE-3947 Fix tests path

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=$(dirname $DIR)
 
-HIDE_STREAM_LOG=true TEST_HOME_DIR="$DIR/tests/" GLOBAL_SKALE_DIR="$DIR/tests/etc/skale" DOTENV_FILEPATH='tests/test-env' py.test --cov=$PROJECT_DIR/ tests/ --ignore=tests/operations/ $@
+HIDE_STREAM_LOG=true TEST_HOME_DIR="$PROJECT_DIR/tests/" GLOBAL_SKALE_DIR="$PROJECT_DIR/tests/etc/skale" DOTENV_FILEPATH='tests/test-env' py.test --cov=$PROJECT_DIR/ tests/ --ignore=tests/operations/ $@
