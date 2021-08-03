38029be42489e942a65ec9c5c97cd817dc713bbc
badrogger <ivan@skalelabs.com>
2021-08-03 21:14:42 +0300

    SKALE-4449 Add changing indication to docker config

--- node_cli/core/host2/docker_config.py
+++ node_cli/core/host2/docker_config.py
@@ -1,10 +1,10 @@
+import enum
+import filecmp
 import json
 import os
 import pathlib
 import time
 import typing
-from collections import namedtuple
-from subprocess import CompletedProcess
 from typing import Optional
 
 from node_cli.configs import (
@@ -38,24 +38,24 @@ class NoSocketFileError(DockerConfigError):
     pass
 
 
-DockerConfigResult = namedtuple(
-    'DockerConfigResult',
-    'status',
-    'data'
-)
+class DockerConfigResult(enum.IntEnum):
+    UNCHANGED = 0
+    CHANGED = 1
 
 
 def ensure_docker_service_config_dir(
         docker_service_dir: Path = DOCKER_SERVICE_CONFIG_DIR
-) -> None:
+) -> DockerConfigResult:
     if not os.path.isdir(docker_service_dir):
         os.makedirs(docker_service_dir, exist_ok=True)
+        return DockerConfigResult.CHANGED
+    return DockerConfigResult.UNCHANGED
 
 
 def ensure_service_overriden_config(
     config_filepath:
     Optional[Path] = DOCKER_SERVICE_CONFIG_PATH
-) -> None:
+) -> DockerConfigResult:
     config = get_content(config_filepath)
     expected_config = '\n'.join(
         ['[Service]', 'ExecStart=', 'ExecStart=/usr/bin/dockerd']
@@ -64,10 +64,12 @@ def ensure_service_overriden_config(
     if not os.path.isfile(config_filepath):
         with open(config_filepath, 'w') as config_file:
             config_file.write(expected_config)
+            return DockerConfigResult.CHANGED
     elif config != expected_config:
         raise OverridenConfigExsitsError(
             f'{config_filepath} already exists'
         )
+    return DockerConfigResult.UNCHANGED
 
 
 def ensure_docker_daemon_config(
@@ -76,20 +78,24 @@ def ensure_docker_daemon_config(
     config = {}
     if os.path.isfile(daemon_config_path):
         with open(daemon_config_path, 'r') as daemon_config:
-            print(daemon_config)
             config = json.load(daemon_config)
+    if config.get('live-restore') is True and \
+       config.get('hosts') == ['unix:///var/lib/skale/docker.sock']:
+        return DockerConfigResult.UNCHANGED
     config.update({
         'live-restore': True,
         'hosts': ['unix:///var/lib/skale/docker.sock']
     })
     with open(daemon_config_path, 'w') as daemon_config:
         config = json.dump(config, daemon_config)
+    return DockerConfigResult.CHANGED
 
 
 def reload_docker_service(
         docker_service_name: str = 'docker'
-) -> CompletedProcess:
-    return run_cmd(['systemctl', 'restart', docker_service_name])
+) -> DockerConfigResult:
+    run_cmd(['systemctl', 'restart', docker_service_name])
+    return DockerConfigResult.CHANGED
 
 
 def link_socket_to_default_path(
@@ -98,13 +104,24 @@ def link_socket_to_default_path(
 ) -> None:
     if not os.path.isfile(socket_path):
         raise NoSocketFileError(f'socket {socket_path} does not exist')
+    if os.path.islink(default_path) and \
+       filecmp.cmp(socket_path, default_path):
+        return DockerConfigResult.UNCHANGED
     os.symlink(socket_path, default_path)
+    return DockerConfigResult.CHANGED
 
 
 def configure_docker() -> None:
-    ensure_docker_service_config_dir()
-    ensure_service_overriden_config()
-    ensure_docker_daemon_config()
-    reload_docker_service()
-    time.sleep(20)
+    pre_restart_tasks = [
+        ensure_docker_service_config_dir,
+        ensure_service_overriden_config,
+        ensure_docker_daemon_config
+    ]
+    results = [
+        task()
+        for task in pre_restart_tasks
+    ]
+    if any(results, DockerConfigResult.CHANGED):
+        reload_docker_service
+        time.sleep(20)
     link_socket_to_default_path()
--- tests/core/host/docker_config_test.py
+++ tests/core/host/docker_config_test.py
@@ -7,6 +7,7 @@ import shutil
 import pytest
 
 from node_cli.core.host2.docker_config import (
+    DockerConfigResult,
     ensure_docker_service_config_dir,
     ensure_service_overriden_config,
     ensure_docker_daemon_config,
@@ -27,28 +28,32 @@ def tmp_dir():
         shutil.rmtree(tmp_dir_path)
 
 
-def test_ensure_docker_service_config_dir(tmp_dir) -> None:
+def test_ensure_docker_service_config_dir(tmp_dir):
     service_config_dir = os.path.join(tmp_dir, 'docker.service.d')
-    ensure_docker_service_config_dir(service_config_dir)
+    r = ensure_docker_service_config_dir(service_config_dir)
     assert os.path.isdir(service_config_dir)
+    assert r == DockerConfigResult.CHANGED
 
-    ensure_docker_service_config_dir(service_config_dir)
+    r = ensure_docker_service_config_dir(service_config_dir)
     assert os.path.isdir(service_config_dir)
+    assert r == DockerConfigResult.UNCHANGED
 
 
 def test_ensure_service_overriden_config(tmp_dir):
     overriden_config_path = os.path.join(tmp_dir, 'override.conf')
-    ensure_service_overriden_config(overriden_config_path)
+    r = ensure_service_overriden_config(overriden_config_path)
     with open(overriden_config_path) as overriden_config_file:
         assert overriden_config_file.read() == '\n'.join(
             ['[Service]', 'ExecStart=', 'ExecStart=/usr/bin/dockerd']
         )
+    assert r == DockerConfigResult.CHANGED
 
-    ensure_service_overriden_config(overriden_config_path)
+    r = ensure_service_overriden_config(overriden_config_path)
     with open(overriden_config_path) as overriden_config_file:
         assert overriden_config_file.read() == '\n'.join(
             ['[Service]', 'ExecStart=', 'ExecStart=/usr/bin/dockerd']
         )
+    assert r == DockerConfigResult.UNCHANGED
 
     with open(overriden_config_path, 'w') as overriden_config_file:
         overriden_config_file.write('Could it be more useless?')
@@ -59,24 +64,26 @@ def test_ensure_service_overriden_config(tmp_dir):
 
 def test_ensure_docker_daemon_config(tmp_dir):
     daemon_config_path = os.path.join(tmp_dir, 'daemon.json')
-    ensure_docker_daemon_config(daemon_config_path)
+    r = ensure_docker_daemon_config(daemon_config_path)
 
     with open(daemon_config_path, 'r') as daemon_config_file:
         conf = json.load(daemon_config_file)
         assert conf['live-restore'] is True
         assert conf['hosts'] == ['unix:///var/lib/skale/docker.sock']
+    assert r == DockerConfigResult.CHANGED
 
     conf.pop('hosts')
     conf['test'] = 'TEST'
     with open(daemon_config_path, 'w') as daemon_config_file:
         json.dump(conf, daemon_config_file)
 
-    ensure_docker_daemon_config(daemon_config_path)
+    r = ensure_docker_daemon_config(daemon_config_path)
     with open(daemon_config_path, 'r') as daemon_config_file:
         conf = json.load(daemon_config_file)
         assert conf['live-restore'] is True
         assert conf['hosts'] == ['unix:///var/lib/skale/docker.sock']
         assert conf['test'] == 'TEST'
+    assert r == DockerConfigResult.CHANGED
 
 
 def test_link_socket_to_default_path(tmp_dir):
@@ -87,6 +94,7 @@ def test_link_socket_to_default_path(tmp_dir):
         link_socket_to_default_path(socket_path, default_path)
 
     pathlib.Path(socket_path).touch()
-    link_socket_to_default_path(socket_path, default_path)
+    r = link_socket_to_default_path(socket_path, default_path)
     assert os.path.islink(default_path)
     assert filecmp.cmp(socket_path, default_path)
+    assert r == DockerConfigResult.CHANGED
