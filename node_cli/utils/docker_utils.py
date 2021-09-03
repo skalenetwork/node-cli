100640e98858c5e150a27a9218aab590c96ae7e9
badrogger <ivan@skalelabs.com>
2021-09-03 12:50:20 +0300

    SKALE-4428 Add additional logs

--- node_cli/utils/docker_utils.py
+++ node_cli/utils/docker_utils.py
@@ -113,10 +113,10 @@ def safe_rm(container: Container, stop_timeout=DOCKER_DEFAULT_STOP_TIMEOUT, **kw
     """
     container_name = container.name
     backup_container_logs(container)
-    logger.debug(
+    logger.info(
         f'Stopping container: {container_name}, timeout: {stop_timeout}')
     container.stop(timeout=stop_timeout)
-    logger.debug(f'Removing container: {container_name}, kwargs: {kwargs}')
+    logger.info(f'Removing container: {container_name}, kwargs: {kwargs}')
     container.remove(**kwargs)
     logger.info(f'Container removed: {container_name}')
 
