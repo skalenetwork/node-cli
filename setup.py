ce1c95bffd0b42472b71bc60d7c784fc32704356
badrogger <ivan@skalelabs.com>
2022-11-23 13:09:58 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -57,8 +57,6 @@ setup(
         "python-dateutil==2.8.2",
         "Jinja2==3.1.2",
         "psutil==5.9.1",
-        "requests==2.28.0",
-        "GitPython==3.1.14",
         "python-dotenv==0.21.0",
         "terminaltables==3.1.10",
         "requests==2.28.1",
