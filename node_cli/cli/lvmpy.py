d41a9401515e255e5e86110701bc9bcb6160323a
badrogger <ivan@skalelabs.com>
2023-03-17 16:45:08 +0000

    Add command to run lvmpy http server

---
+++ node_cli/cli/lvmpy.py
@@ -0,0 +1,49 @@
+#   -*- coding: utf-8 -*-
+#
+#   This file is part of node-cli
+#
+#   Copyright (C) 2020 SKALE Labs
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import click
+from node_cli.utils.texts import Texts
+
+from node_cli.lvmpy.app import main as run_lvmpy
+from node_cli.utils.helper import abort_if_false
+
+G_TEXTS = Texts()
+TEXTS = G_TEXTS['lvmpy']
+
+
+@click.group()
+def lvmpy_cli():
+    pass
+
+
+@lvmpy_cli.group('lvmpy', help=TEXTS['help'])
+def health():
+    pass
+
+
+@health.command(help=TEXTS['lvmpy']['help'])
+@click.option(
+    '--yes',
+    is_flag=True,
+    callback=abort_if_false,
+    expose_value=False,
+    prompt=TEXTS['lvmpy']['prompt']
+)
+def run(all):
+    run_lvmpy()
