da47f6b61b9659de739707977f663c122d3a421f
Alex Sheverdin <alex@skalelabs.com>
2023-04-07 18:40:05 +0100

    Add distro to dependencies

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     install_requires=[
         "click==8.1.3",
         "PyInstaller==5.6.2",
+        "distro==1.4.0",
         "docker==6.0.1",
         "texttable==1.6.4",
         "python-dateutil==2.8.2",
