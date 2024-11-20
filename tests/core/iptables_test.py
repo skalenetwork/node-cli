0006b95bb83896504f71120a23e86213ea927857
badrogger <ivan@skalelabs.com>
2024-11-20 15:29:28 +0000

    Remove allow_ssh function

--- tests/core/iptables_test.py
+++ tests/core/iptables_test.py
@@ -2,7 +2,6 @@ import socket
 
 import mock
 
-from node_cli.core.iptables import allow_ssh
 from node_cli.utils.helper import get_ssh_port
 
 
@@ -11,9 +10,3 @@ def test_get_ssh_port():
     assert get_ssh_port('http') == 80
     with mock.patch.object(socket, 'getservbyname', side_effect=OSError):
         assert get_ssh_port() == 22
-
-
-def test_allow_ssh():
-    chain = mock.Mock()
-    chain.rules = []
-    allow_ssh(chain)
