202d68e2949704be46d718c1dc81bd93e96fe7e4
badrogger <ivan@skalelabs.com>
2024-11-28 22:11:00 +0000

    Remove duplicate setup_firewall execution

--- node_cli/operations/base.py
+++ node_cli/operations/base.py
@@ -46,8 +46,6 @@ from node_cli.operations.volume import (
 from node_cli.operations.docker_lvmpy import lvmpy_install  # noqa
 from node_cli.operations.skale_node import download_skale_node, sync_skale_node, update_images
 from node_cli.core.checks import CheckType, run_checks as run_host_checks
-# from node_cli.core.iptables import configure_iptables
-from node_cli.core.nftables import configure_nftables
 from node_cli.core.schains import update_node_cli_schain_status, cleanup_sync_datadir
 from node_cli.utils.docker_utils import (
     compose_rm,
@@ -115,8 +113,6 @@ def update(env_filepath: str, env: Dict) -> None:
     backup_old_contracts()
     download_contracts(env)
 
-    configure_nftables()
-
     lvmpy_install(env)
     generate_nginx_config()
 
@@ -166,7 +162,6 @@ def init(env_filepath: str, env: dict) -> bool:
 
     configure_filebeat()
     configure_flask()
-    configure_nftables()
     generate_nginx_config()
 
     lvmpy_install(env)
@@ -323,7 +318,6 @@ def restore(env, backup_path, config_only=False):
         configure_docker()
 
     link_env_file()
-    configure_nftables()
     lvmpy_install(env)
     init_shared_space_volume(env['ENV_TYPE'])
 
