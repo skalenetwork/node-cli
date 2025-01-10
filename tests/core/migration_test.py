efa7355d857635dea542462122b99dcb7f524795
badrogger <ivan@skalelabs.com>
2025-01-10 16:12:46 +0000

    Fix tests

--- tests/core/migration_test.py
+++ tests/core/migration_test.py
@@ -40,6 +40,6 @@ def test_migration(base_rules):
     migrate()
     res = run_cmd(['iptables', '-S'])
     output = res.stdout.decode('utf-8')
-    assert output == f'-P INPUT ACCEPT\n-P FORWARD ACCEPT\n-P OUTPUT ACCEPT\n-N {CUSTOM_CHAIN_NAME}\n-A {CUSTOM_CHAIN_NAME} -p tcp -m tcp --dport 2222 -j ACCEPT\n'  # noqa
+    assert output == f'-P INPUT ACCEPT\n-P FORWARD ACCEPT\n-P OUTPUT ACCEPT\n-N {CUSTOM_CHAIN_NAME}\n-A INPUT -p tcp -m tcp --dport 22 -j ACCEPT\n-A INPUT -p udp -m udp --dport 53 -j ACCEPT\n-A {CUSTOM_CHAIN_NAME} -p tcp -m tcp --dport 2222 -j ACCEPT\n'  # noqa
     nft = NFTablesManager(family='ip', table='filter')
-    assert nft.get_rules(chain='INPUT') == []
+    assert nft.get_rules(chain='INPUT') == [{'family': 'ip', 'table': 'filter', 'chain': 'INPUT', 'handle': 5, 'expr': [{'match': {'op': '==', 'left': {'payload': {'protocol': 'tcp', 'field': 'dport'}}, 'right': 22}}, {'counter': {'packets': 0, 'bytes': 0}}, {'accept': None}]}, {'family': 'ip', 'table': 'filter', 'chain': 'INPUT', 'handle': 9, 'expr': [{'match': {'op': '==', 'left': {'payload': {'protocol': 'udp', 'field': 'dport'}}, 'right': 53}}, {'counter': {'packets': 0, 'bytes': 0}}, {'accept': None}]}]  # noqa
