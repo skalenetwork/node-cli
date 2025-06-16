7a816b752f86bf8d0a09007ad4138e2473560dc5
badrogger <ivan@skalelabs.com>
2025-06-16 18:41:35 +0100

    Update to helper-scripts with redis

--- scripts/run_redis.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-docker rm -f redis || true
-export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docker run -v $DIR/../tests/redis-conf:/config --network=host --name=redis -d redis:6.0-alpine
