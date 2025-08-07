09594494b6ababbb20e0383ca5328bbfbfca8bde
Alex Sheverdin <alex@skalelabs.com>
2025-08-07 15:57:45 +0100

    Add 'set-domain-name' to 'ROUTS' for FAIR node

--- node_cli/configs/routes.py
+++ node_cli/configs/routes.py
@@ -40,7 +40,7 @@ ROUTES = {
         'schains': ['config', 'list', 'dkg-statuses', 'firewall-rules', 'repair', 'get'],
         'ssl': ['status', 'upload'],
         'wallet': ['info', 'send-eth'],
-        'fair-node': ['info', 'register', 'change-ip', 'exit'],
+        'fair-node': ['info', 'register', 'set-domain-name', 'change-ip', 'exit'],
         'fair-chain': ['record', 'checks'],
     }
 }
