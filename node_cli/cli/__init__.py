7f0f2f0bad639d7e573459d7fab4c9b4f9075cf1
Alex Sheverdin <alex@skalelabs.com>
2025-11-24 16:26:19 +0000

    Use importlib.metadata to get package version

--- node_cli/cli/__init__.py
+++ node_cli/cli/__init__.py
@@ -1,4 +1,9 @@
-__version__ = '3.2.0'
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("node-cli")
+except PackageNotFoundError:
+    __version__ = "0.0.0-dev"
 
 if __name__ == '__main__':
     print(__version__)
