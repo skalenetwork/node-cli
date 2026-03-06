99e05c7c2158fe135280fd72493ac13e93bb7952
Dmytro <dmitry.tkk@gmail.com>
2026-03-06 12:37:38 +0000

    Add internal settings generation for passive node

--- node_cli/core/node.py
+++ node_cli/core/node.py
@@ -201,6 +201,7 @@ def init_passive(
     config_file: str, indexer: bool, archive: bool, snapshot: bool, snapshot_from: Optional[str]
 ) -> None:
     node_mode = NodeMode.PASSIVE
+    save_internal_settings(node_type=NodeType.SKALE, node_mode=node_mode)
     settings = validate_and_save_node_settings(config_file, NodeType.SKALE, node_mode)
     compose_env = compose_node_env(node_type=NodeType.SKALE, node_mode=node_mode)
     init_passive_op(
@@ -225,6 +226,7 @@ def update_passive(config_file: str) -> None:
     prev_version = CliMetaManager().get_meta_info().version
     if (__version__ == 'test' or __version__.startswith('2.6')) and prev_version == '2.5.0':
         migrate_2_6()
+    save_internal_settings(node_type=NodeType.SKALE, node_mode=NodeMode.PASSIVE)
     settings = validate_and_save_node_settings(config_file, NodeType.SKALE, NodeMode.PASSIVE)
     compose_env = compose_node_env(node_type=NodeType.SKALE, node_mode=NodeMode.PASSIVE)
     update_ok = update_passive_op(settings=settings, compose_env=compose_env)
